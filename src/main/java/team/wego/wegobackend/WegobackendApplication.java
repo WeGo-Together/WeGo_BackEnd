a1bf297ae6148514469846e0fe0a77d9a5235c26
Be-HinD <kr100475@gmail.com>
2025-11-25 22:21:16 +0900

    :tada: init : Springboot 프로젝트 초기 설정 - Springboot : 3.5.8 - Java : 21 - Build Tool : Gradle

---
+++ src/main/java/team/wego/wegobackend/WegobackendApplication.java
@@ -0,0 +1,13 @@
+package team.wego.wegobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegobackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WegobackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/wego/wegobackend/WegobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package team.wego.wegobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WegobackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
