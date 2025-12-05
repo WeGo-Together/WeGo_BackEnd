f8f17ddbda90276c10eefb41da3d58947390308f
LimdaeIl <optional94@naver.com>
2025-12-05 15:30:06 +0900

    🔧config: S3 설정 클래스 작성[#23]
    S3Client를 스프링 빈으로 하나 만들어서 전역에서 쓰게 해주는 설정 클래스입니다.
    
    S3Client 인스턴스를 만들기 위해 빌더(builder) 객체로 지역(region), 자격 증명 공급 방식(credentialsProvider) 설정을 기반으로 생성된 S3Client 객체를 스프링 빈으로 등록합니다.
    
    비용적인 문제로 인해 Ligthsail 인스턴스를 사용하기 때문에, IAM Role, Profile Provider 대체하고, 민감한 접속 정보들은 모두 직접 관리하고 있습니다.

---
+++ src/main/java/team/wego/wegobackend/common/infrastructure/aws/s3/S3Config.java
@@ -0,0 +1,32 @@
+package team.wego.wegobackend.common.infrastructure.aws.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Value("${aws.s3.access-key-id}")
+    private String accessKeyId;
+
+    @Value("${aws.s3.secure-access-key}")
+    private String secretAccessKey;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+}
