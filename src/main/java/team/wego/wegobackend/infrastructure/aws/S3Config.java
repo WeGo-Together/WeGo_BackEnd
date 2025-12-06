2538d2741cdee17fd6058f74a5cee1b88a03c5be
LimdaeIl <optional94@naver.com>
2025-12-06 22:00:21 +0900

    ♻️refactor: S3 Config를 wegobackend/infratructure/aws 로 이동[#27]

--- src/main/java/team/wego/wegobackend/infrastructure/aws/S3Config.java
+++ src/main/java/team/wego/wegobackend/infrastructure/aws/S3Config.java
@@ -1,4 +1,4 @@
-package team.wego.wegobackend.image.infrastructure;
+package team.wego.wegobackend.infrastructure.aws;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
