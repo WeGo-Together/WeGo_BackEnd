fcc155a10a1fd8f349e2be1b6e03f369928d007e
LimdaeIl <optional94@naver.com>
2025-12-06 21:20:20 +0900

    ✨️feat: 공통 API 성공 응답 구현[#25]
    Description
    - code: HttpStatus Code (2XX)
    - message: 응답 메시지
    - data: 실제 응답 데이터(JSON)
    
    - https://github.com/WeGo-Together/WeGo_BackEnd/wiki/API-Overview

---
+++ src/main/java/team/wego/wegobackend/common/response/ApiResponse.java
@@ -0,0 +1,31 @@
+package team.wego.wegobackend.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiResponse<T>(
+        String code,
+        String message,
+        T data
+) {
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>("200", "요청이 정상적으로 처리되었습니다.", data);
+    }
+
+    public static <T> ApiResponse<T> success(String code, String message, T data) {
+        return new ApiResponse<>(code, message, data);
+    }
+
+    public static <T> ApiResponse<T> success(String code, String message) {
+        return new ApiResponse<>(code, message, null);
+    }
+
+    public static <T> ApiResponse<T> error(String code, String message) {
+        return new ApiResponse<>(code, message, null);
+    }
+
+    public static <T> ApiResponse<T> error(String code, String message, T data) {
+        return new ApiResponse<>(code, message, data);
+    }
+}
