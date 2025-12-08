eaf5506079f8ac505ee60bd9efcc786ad9cec29f
LimdaeIl <optional94@naver.com>
2025-12-08 16:23:22 +0900

    ✨️feat: 공통 응답 수정[#34]
    ApiResponse에서 HTTP Status 상태 표시를 제거합니다.

--- src/main/java/team/wego/wegobackend/common/response/ApiResponse.java
+++ src/main/java/team/wego/wegobackend/common/response/ApiResponse.java
@@ -4,37 +4,27 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T>(
-        String code,
         String message,
         T data
 ) {
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>("200", "요청이 정상적으로 처리되었습니다.", data);
+        return new ApiResponse<>( "요청이 정상적으로 처리되었습니다.", data);
     }
 
-    public static <T> ApiResponse<T> success(String code, String message, T data) {
-        return new ApiResponse<>(code, message, data);
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(message, data);
     }
 
-    public static <T> ApiResponse<T> success(String code, String message) {
-        return new ApiResponse<>(code, message, null);
+    public static <T> ApiResponse<T> success( String message) {
+        return new ApiResponse<>( message, null);
     }
 
-    public static <T> ApiResponse<T> success(int code, String message, T data) {
-        return new ApiResponse<>(String.valueOf(code), message, data);
+    public static <T> ApiResponse<T> error( String message) {
+        return new ApiResponse<>(message, null);
     }
 
-    public static <T> ApiResponse<T> success(int code, String message) {
-        return new ApiResponse<>(String.valueOf(code), message, null);
-    }
-
-
-    public static <T> ApiResponse<T> error(String code, String message) {
-        return new ApiResponse<>(code, message, null);
-    }
-
-    public static <T> ApiResponse<T> error(String code, String message, T data) {
-        return new ApiResponse<>(code, message, data);
+    public static <T> ApiResponse<T> error(String message, T data) {
+        return new ApiResponse<>(message, data);
     }
 }
