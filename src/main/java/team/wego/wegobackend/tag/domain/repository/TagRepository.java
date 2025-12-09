7e250419c7e34a59bcc2343cb6649d3d1d430536
LimdaeIl <optional94@naver.com>
2025-12-09 14:05:04 +0900

    ✨️feat: 이미 존재하는 태그 반환받는 메서드 구현[#41]

--- src/main/java/team/wego/wegobackend/tag/domain/repository/TagRepository.java
+++ src/main/java/team/wego/wegobackend/tag/domain/repository/TagRepository.java
@@ -1,8 +1,10 @@
 package team.wego.wegobackend.tag.domain.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.wego.wegobackend.tag.domain.entity.Tag;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findByNameIn(List<String> tagNames);
 
 }
