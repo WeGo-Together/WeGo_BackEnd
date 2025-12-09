ee5a4235e25c5e20e3cec4f3a3b75b502c52eaa4
LimdaeIl <optional94@naver.com>
2025-12-09 17:26:53 +0900

    ♻️refactor: 이미지 에러 메시지 출력하도록 수정[#41]

--- src/main/java/team/wego/wegobackend/group/domain/exception/GroupErrorCode.java
+++ src/main/java/team/wego/wegobackend/group/domain/exception/GroupErrorCode.java
@@ -13,7 +13,7 @@ public enum GroupErrorCode implements ErrorCode {
     INVALID_TIME_RANGE(HttpStatus.BAD_REQUEST, "모임: 종료 시간은 시작 시간보다 뒤여야 합니다."),
     INVALID_MAX_PARTICIPANTS(HttpStatus.BAD_REQUEST, "모임: 최대 인원은 최소 2명 이상이어야 합니다."),
     TAG_NOT_FOUND(HttpStatus.NOT_FOUND, "모임: 요청한 태그를 찾을 수 없습니다."),
-    IMAGE_UPLOAD_FAILED(HttpStatus.BAD_REQUEST, "모임: 이미지 업로드 요청에 실패했습니다."),
+    IMAGE_UPLOAD_FAILED(HttpStatus.BAD_REQUEST, "모임: 이미지 업로드 요청에 실패했습니다: %s"),
     GROUP_NOT_FOUND_BY_ID(HttpStatus.NOT_FOUND, "모임: 모임을 찾을 수 없습니다. 모임 ID: %s"),
     IMAGE_UPLOAD_EXCEED(HttpStatus.BAD_REQUEST, "모임: 모임 이미지는 최대 3개 입니다. 현재 이미지 수: %s");
 
