f69a4a6c473081c647d3829c4b2c063934af0da8
Be-HinD <kr100475@gmail.com>
2025-12-10 01:24:24 +0900

    안쓰는 패키지 제거

--- src/main/java/team/wego/wegobackend/user/application/dto/request/ProfileUpdateRequest.java
+++ src/main/java/team/wego/wegobackend/user/application/dto/request/ProfileUpdateRequest.java
@@ -1,6 +1,5 @@
 package team.wego.wegobackend.user.application.dto.request;
 
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
