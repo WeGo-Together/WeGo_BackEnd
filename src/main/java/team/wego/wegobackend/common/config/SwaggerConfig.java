f1cdaa02434ac71eb78be0d981132ba574151d8b
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2025-12-12 01:57:42 +0900

    :bug:fix: Swagger cors 오류 조치 (#90)

--- src/main/java/team/wego/wegobackend/common/config/SwaggerConfig.java
+++ src/main/java/team/wego/wegobackend/common/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,6 +29,9 @@ public class SwaggerConfig {
             .components(new Components()
                 .addSecuritySchemes("bearerAuth", securityScheme))
             .security(List.of(securityRequirement))
+            .servers(List.of(
+                new Server().url("/").description("Current Server")
+            ))
             .info(apiInfo());
     }
 
