3e0210352a1d656826bc8580a7339a2ed149b280
LimdaeIl <optional94@naver.com>
2025-12-19 16:56:41 +0900

    ✨feat: 이미지 업로드를 위해 멀티파트 폼 데이터를 사용 [#138]
    이미지 업로드 엔드포인트가 멀티파트 폼 데이터를 허용하도록 구성하여 이미지 파일 전송이 가능하도록 합니다.
    또한, width() 및 height() 함수를 사용하여 이미지의 너비와 높이를 가져옵니다.

--- src/main/java/team/wego/wegobackend/group/v2/infrastructure/redis/PreUploadedGroupImageRedisConfig.java
+++ src/main/java/team/wego/wegobackend/group/v2/infrastructure/redis/PreUploadedGroupImageRedisConfig.java
@@ -37,3 +37,4 @@ public class PreUploadedGroupImageRedisConfig {
         return template;
     }
 }
+
--- src/main/java/team/wego/wegobackend/group/v2/presentation/GroupImageV2Controller.java
+++ src/main/java/team/wego/wegobackend/group/v2/presentation/GroupImageV2Controller.java
@@ -3,6 +3,7 @@ package team.wego.wegobackend.group.v2.presentation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +24,7 @@ public class GroupImageV2Controller {
     private final GroupImageV2PreUploadService preUploadService;
 
 
-    @PostMapping("/upload")
+    @PostMapping(value="/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<PreUploadGroupImageV2Response>> uploadImages(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestParam("images") List<MultipartFile> images
