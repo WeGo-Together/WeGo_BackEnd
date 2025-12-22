bd8cd835a8a9886b5eebaef4a04aaa00df33dd5a
LimdaeIl <optional94@naver.com>
2025-12-22 22:44:09 +0900

    ♻️refactor: 모임 .http 테스트 리팩토링[#148]

--- src/main/java/team/wego/wegobackend/group/v2/application/dto/response/AttendanceGroupV2Response.java
+++ src/main/java/team/wego/wegobackend/group/v2/application/dto/response/AttendanceGroupV2Response.java
@@ -3,11 +3,13 @@ package team.wego.wegobackend.group.v2.application.dto.response;
 import java.time.LocalDateTime;
 import team.wego.wegobackend.group.v2.application.dto.common.MyMembership;
 import team.wego.wegobackend.group.v2.domain.entity.GroupV2;
+import team.wego.wegobackend.group.v2.domain.entity.GroupV2JoinPolicy;
 import team.wego.wegobackend.group.v2.domain.entity.GroupV2Status;
 
 public record AttendGroupV2Response(
         Long groupId,
         GroupV2Status groupStatus,
+        GroupV2JoinPolicy joinPolicy,
         long participantCount,
         int maxParticipants,
         MyMembership myMembership,
@@ -22,6 +24,7 @@ public record AttendGroupV2Response(
         return new AttendGroupV2Response(
                 group.getId(),
                 group.getStatus(),
+                group.getJoinPolicy(),
                 participantCount,
                 group.getMaxParticipants(),
                 myMembership,
